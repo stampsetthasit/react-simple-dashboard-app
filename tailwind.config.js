e4cdac281373c704ca3afd61db75b97eec8c6806
Setthasit Poosawat <setthasit.developer@gmail.com>
2024-09-23 09:50:56 +0700

    Edit: additional colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-blue-primary": "#02BCC8",
+        "light-blue-secondary": "#015C62",
+        "gray-primary": "#9A9A9A",
+        "red-primary": "#C80202",
+      },
+    },
   },
   plugins: [],
 };
